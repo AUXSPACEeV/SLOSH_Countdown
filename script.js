19e724705a968cdf3f0927cf1b8c38a3eab20575
joldjunge <124197990+joldjunge@users.noreply.github.com>
2025-03-09 23:36:57 +0100

    Fix even more safari incompatibilities

--- script.js
+++ script.js
@@ -145,3 +145,12 @@ document.addEventListener('gesturechange', function(event) {
 document.addEventListener('gestureend', function(event) {
   event.preventDefault();
 }, { passive: false });
+
+let lastTouchEnd = 0;
+document.addEventListener("touchend", function (event) {
+  let now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, { passive: false });
