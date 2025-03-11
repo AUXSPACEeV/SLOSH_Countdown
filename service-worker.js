c6c972b56b044e696064d66b49f321e5ecfe2bad
joldjunge <124197990+joldjunge@users.noreply.github.com>
2025-03-11 13:41:38 +0100

    Fix progressive web app

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,10 @@
 // noinspection JSUnusedLocalSymbols
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("pwa-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/styles.css"]);
+    })
+  );
 });
 
 // noinspection JSUnusedLocalSymbols
@@ -8,6 +12,10 @@ self.addEventListener('activate', (event) => {
   console.log('Service Worker activated');
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
