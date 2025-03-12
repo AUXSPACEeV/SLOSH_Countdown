b35045ed7a8fa303e5d95a145cde84cf26288993
joldjunge <124197990+joldjunge@users.noreply.github.com>
2025-03-12 13:44:22 +0100

    Switch to network first caching

--- web-app/service-worker.js
+++ web-app/service-worker.js
@@ -1,21 +1,51 @@
-// noinspection JSUnusedLocalSymbols
+const CACHE_VERSION = "v1";
+const STATIC_CACHE_NAME = `static-${CACHE_VERSION}`;
+const DYNAMIC_CACHE_NAME = `dynamic-${CACHE_VERSION}`;
+
 self.addEventListener("install", (event) => {
+  console.log("Service Worker: Installed");
   event.waitUntil(
-    caches.open("pwa-cache").then((cache) => {
-      return cache.addAll(["./", "./index.html", "./styles.css"]);
+    caches.open(STATIC_CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./script.js",
+        "./styles.css",
+        "./web-app/manifest.json",
+      ]);
     })
   );
 });
 
-// noinspection JSUnusedLocalSymbols
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activated");
+  const currentCaches = [STATIC_CACHE_NAME, DYNAMIC_CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!currentCaches.includes(cacheName)) {
+            console.log("Service Worker: Clearing old cache", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
